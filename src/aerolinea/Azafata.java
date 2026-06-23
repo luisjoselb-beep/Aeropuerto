0d948cc2ebec27294b3c5f871399402fe1c54b1a
Usuario <Usuario@DESKTOP-8NJNC15>
2026-06-22 21:15:25 -0400

    Maquetacion Base y adelanto

--- src/aerolinea/Azafata.java
+++ src/aerolinea/Azafata.java
@@ -2,7 +2,18 @@
 package aerolinea;
 
 public class Azafata extends Personas {
+    
+    private double altura;
+    private int idiomas;
 
+    public Azafata(double altura, int idiomas, int id, String nombre, int edad, Vuelos vuelo, String categoria) {
+        super(id, nombre, edad, vuelo, categoria);
+        this.altura = altura;
+        this.idiomas = idiomas;
+    }
+
+    
+    
     @Override
     public void mostrar() {
         throw new UnsupportedOperationException("Not supported yet."); 
@@ -12,8 +23,25 @@ public class Azafata extends Personas {
     public void calcularCategoria() {
         throw new UnsupportedOperationException("Not supported yet."); 
     }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public int getIdiomas() {
+        return idiomas;
+    }
+
+    public void setIdiomas(int idiomas) {
+        this.idiomas = idiomas;
+    }
     
     
+
     
     
     
---
+++ src/aerolinea/Piloto.java
@@ -0,0 +1,36 @@
+
+package aerolinea;
+
+
+public class Piloto extends Personas {
+    //atributo
+    private int horas;
+    //constructor
+    public Piloto(int horas, int id, String nombre, int edad, Vuelos vuelo, String categoria) {
+        super(id, nombre, edad, vuelo, categoria);
+        this.horas = horas;
+    }
+    
+    
+    @Override
+    public void mostrar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void calcularCategoria() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    //getters y setters
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+    
+    
+    
+}
